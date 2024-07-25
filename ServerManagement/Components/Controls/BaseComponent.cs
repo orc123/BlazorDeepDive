cc47594ac52b3acdd2584efefd90bed140de55cb
Phạm Ngọc Đông <dong.orc12@gmail.com>
2024-07-25 13:54:02 +0700

    End Session 7

---
+++ ServerManagement/Components/Controls/BaseComponent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace ServerManagement.Components.Controls;
+
+public class BaseComponent : ComponentBase
+{
+    protected bool isFirstRender { get; set; } = true;
+
+    protected override void OnAfterRender(bool firstRender)
+    {
+        base.OnAfterRender(firstRender);
+
+        isFirstRender = firstRender;
+    }
+
+}
